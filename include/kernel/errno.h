e5d1e4829e8a8868863f548d8c64309967f3c8f7
JamesB-Wood <james.brian.wood@gmail.com>
2025-04-21 15:46:26 +1000

    added errno.h

---
+++ include/kernel/errno.h
@@ -0,0 +1,18 @@
+#ifndef ERRNO_H_
+#define ERRNO_H_
+
+#define EPERM 1    /* Operation not permitted */
+#define ENOENT 2   /* No such file or directory */
+#define EIO 5      /* I/O error */
+#define ENXIO 6    /* No such device or address */
+#define E2BIG 7    /* Argument list too long */
+#define EAGAIN 11  /* Try again */
+#define ENOMEM 12  /* Out of memory */
+#define EACCES 13  /* Permission denied */
+#define EFAULT 14  /* Bad address */
+#define ENOTBLK 15 /* Block device required */
+#define EBUSY 16   /* Device or resource busy */
+#define ENODEV 19  /* No such device */
+#define EINVAL 22  /* Invalid argument */
+
+#endif
