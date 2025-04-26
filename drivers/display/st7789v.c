eab77ffdaee36af7ce2d8bc679dc0f326565fe15
Benksur <tyjbehnke1@gmail.com>
2025-04-26 20:59:49 +1000

    added example task

--- drivers/display/st7789v.c
+++ drivers/display/st7789v.c
@@ -66,3 +66,40 @@ static void my_lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd
     st7789v_write_data_buffer(param, pixel_count);
 }
 
+/*
+example display task
+void task_display(void const *argument) {
+    st7789v_io_init();
+    lv_init();
+    lv_display_t *lcd_disp = lv_st7789_create(LCD_H_RES, LCD_V_RES, LV_LCD_FLAG_NONE, lcd_send_cmd, lcd_send_color);
+    
+    lv_color_t * buf1 = NULL;
+    lv_color_t * buf2 = NULL;
+
+    uint32_t buf_size = LCD_H_RES * LCD_V_RES / 10 * lv_color_format_get_size(lv_display_get_color_format(lcd_disp));
+
+    buf1 = lv_malloc(buf_size);
+    if(buf1 == NULL) {
+        LV_LOG_ERROR("display draw buffer malloc failed");
+        return;
+    }
+
+    buf2 = lv_malloc(buf_size);
+    if(buf2 == NULL) {
+        LV_LOG_ERROR("display buffer malloc failed");
+        lv_free(buf1);
+        return;
+    }
+    lv_display_set_buffers(lcd_disp, buf1, buf2, buf_size, LV_DISPLAY_RENDER_MODE_PARTIAL);
+
+    ui_init(lcd_disp);
+
+    for(;;) {
+        lv_timer_handler();
+        osDelay(10);
+    }
+
+
+
+*/
+
